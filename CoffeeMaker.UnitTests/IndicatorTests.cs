b7f3e590720760357c09ab5e285fbaa0de9d43a9
Mark Seemann <mark@ploeh.dk>
2013-10-07 19:38:54 +0200

    Introduced the Indicator class,
    which will used to control the indicator light.

---
+++ CoffeeMaker.UnitTests/IndicatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Ploeh.Samples.CoffeeMaker;
+using Xunit;
+
+namespace Ploeh.Samples.CoffeeMaker.UnitTests
+{
+    public class IndicatorTests
+    {
+        [Theory, TestConventions]
+        public void SutIsObververOfBoilerStatus(Indicator sut)
+        {
+            Assert.IsAssignableFrom<IObserver<BoilerStatus>>(sut);
+        }
+    }
+}
