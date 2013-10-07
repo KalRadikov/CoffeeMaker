d0869a49a5d231d68c74e6cf75be397a2e40e468
Mark Seemann <mark@ploeh.dk>
2013-10-07 16:58:08 +0200

    Introduced Boiler,
    which will control the hardware boiler.

---
+++ CoffeeMaker.UnitTests/BoilerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Ploeh.Samples.CoffeeMaker;
+
+namespace Ploeh.Samples.CoffeeMaker.UnitTests
+{
+    public class BoilerTests
+    {
+        [Theory, TestConventions]
+        public void SutIsObserverOfBrewButtonStatus(Boiler sut)
+        {
+            Assert.IsAssignableFrom<IObserver<BrewButtonStatus>>(sut);
+        }
+    }
+}
---
+++ CoffeeMaker/Boiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ploeh.Samples.CoffeeMaker
+{
+    public class Boiler : IObserver<BrewButtonStatus>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(BrewButtonStatus value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
