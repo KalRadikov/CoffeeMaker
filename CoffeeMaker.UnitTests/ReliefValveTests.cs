11ff8a6d624df71ead945da38124f0dd11b95160
Mark Seemann <mark@ploeh.dk>
2013-10-07 20:21:59 +0200

    Introduced ReliefValve class,
    which will be used to control the 'hardware' relief valve.

---
+++ CoffeeMaker.UnitTests/ReliefValveTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.Samples.CoffeeMaker;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Ploeh.Samples.CoffeeMaker.UnitTests
+{
+    public class ReliefValveTests
+    {
+        [Theory, TestConventions]
+        public void SutIsObserverOfWarmerPlateStatus(ReliefValve sut)
+        {
+            Assert.IsAssignableFrom<IObserver<WarmerPlateStatus>>(sut);
+        }
+    }
+}
